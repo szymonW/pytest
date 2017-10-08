1b1cc0459d633d96c7538ec03d25cf35fb4b20e2
szymonW <szymon.wyderka@gmail.com>
2017-10-08 13:05:23 +0200

    first scenarios

---
+++ tests/scenarios.py
@@ -0,0 +1,14 @@
+import time
+
+def test_first():
+    assert True
+    time.sleep(1)
+
+
+def test_drugi():
+    assert True
+    time.sleep(2)
+
+def test_trzeci():
+    assert True
+    time.sleep(3)
